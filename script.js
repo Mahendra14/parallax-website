d86eec951fbfb83d74e6247ca3cac4df39eeac83
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-28 16:38:03 +0530

    added a timeline example

---
+++ script.js
@@ -0,0 +1,6 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+//timeline to create multiple animations
+
+//ex:  timeline.to(".text",5,{x:500})
+//makes the text slide to 500 at the end of 5 sec
